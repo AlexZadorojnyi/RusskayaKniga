e8634d5526a91ad6ea37519608d87c886aaea2c3
AlexZadorojnyi <alexzadorojnyi@gmail.com>
2018-06-15 21:49:54 -0400

    Added cycling promotional slides

--- js/main.js
+++ js/main.js
@@ -14,3 +14,26 @@ function changeLang() {
 		window.location = newUrl;
 	}
 }
+
+var slides = document.getElementsByClassName("promoSlides");
+var slideIndex = 1;
+var slideDuration = 5000;
+var myVar = setInterval(cycleSlides, slideDuration);
+
+function cycleSlides() {	
+	if (slideIndex > slides.length - 1) {
+		slideIndex = 0;
+	} else if (slideIndex < 0) {
+		slideIndex = slides.length - 1;
+	}
+	
+	for (var i = 0; i < slides.length; i++){
+		if (i == slideIndex) {
+			slides[i].style.display = "block";
+		} else {
+			slides[i].style.display = "none";
+		}
+	}
+	
+	slideIndex++;
+}
