1d90d04978594bc8f945fe394e3c40785d0e33cb
Alexander Zadorojnyi <azadoroj.scs.ryerson.ca>
2018-07-02 23:29:12 -0400

    Implemented independent slider scaling

--- js/jquery.js
+++ js/jquery.js
@@ -2,58 +2,58 @@ $(document).ready(function(){
 	
 	//Width of one book element without padding
 	var bookInnerWidth = $(".book").outerWidth();
-	//Number of book elements
-	var bookNum = $(".book").length;
-	//Width of the book container element
-	var containerWidth;
-	//Width of one book element with padding
-	var bookOuterWidth;
-	//Total width of all book elements with padding
-	var totalWidth;
+	
 	//Sets margin width based on container parameters
 	setMargins();
 	
 	//Scroll books <<LEFT<<
 	$(".scrollLeft").click(function(){
-		var thisClass = this.classList[0];
+		var thisClass = this.classList[1];
 		//console.log(thisClass);
-		if(Math.round((bookOuterWidth * $("." + thisClass + ".book").length - containerWidth) + $("." + thisClass + ".book:first").position().left - 1 - bookOuterWidth) >= 0){
-			$("." + thisClass + ".book").animate({right: "+=" + bookOuterWidth + "px"}, "slow");
+		if(Math.round(($("." + thisClass + ".book").outerWidth(true) * $("." + thisClass + ".book").length - $("." + thisClass + ".bookContainer").innerWidth()) + $("." + thisClass + ".book:first").position().left - 1 - $("." + thisClass + ".book").outerWidth(true)) >= 0){
+			$("." + thisClass + ".book").animate({right: "+=" + $("." + thisClass + ".book").outerWidth(true) + "px"}, "slow");
 		} else {
-			console.log("Reached end of list");
+			console.log(thisClass + " reached end of list");
 		}
 	});
 	
 	//Scroll books >>RIGHT>>
 	$(".scrollRight").click(function(){
-		var thisClass = this.classList[0];
+		var thisClass = this.classList[1];
 		//console.log(thisClass);
-		if(Math.round($("." + thisClass + ".book:first").position().left - 1 + bookOuterWidth) <= 0){
-			$("." + thisClass + ".book").animate({right: "-=" + bookOuterWidth + "px"}, "slow");
+		if(Math.round($("." + thisClass + ".book:first").position().left - 1 + $("." + thisClass + ".book").outerWidth(true)) <= 0){
+			$("." + thisClass + ".book").animate({right: "-=" + $("." + thisClass + ".book").outerWidth(true) + "px"}, "slow");
 		} else {
-			console.log("Reached start of list");
+			console.log(thisClass + " reached start of list");
 		}
 	});
 	
 	//Sets margin width based on container parameters
 	function setMargins() {
-		var i = 0;
-		var r = 0;
-		
-		containerWidth = $(".bookContainer").innerWidth();
-		
-		do {
-			i++;
-			r = containerWidth - (bookInnerWidth * i);
-		} while (r > bookInnerWidth && i <= bookNum);
-		
-		$(".book").css("margin-left", (r / i / 2) + "px");
-		$(".book").css("margin-right", (r / i / 2) + "px");
-		
-		bookOuterWidth = $(".book").outerWidth(true);
-		totalWidth = bookOuterWidth * bookNum;
-		
-		console.log("Container width: " + containerWidth + "px\nBook width: " + bookInnerWidth + "px\nFits: " + i + "/" + bookNum + " books at once\nRemainder: " + r + "px\nMargins: " + r + "/" + i + "=" + (r / i / 2) + "px");
+		$(".bookContainer").each(function(){
+			//Number of book elements
+			var bookNum = $(this).children(".book").length;
+			//Width of the book container element
+			var containerWidth = $(this).innerWidth();
+			
+			var i = 0;
+			var r = 0;
+			
+			do {
+				i++;
+				r = containerWidth - (bookInnerWidth * i);
+			} while (r > bookInnerWidth && i < bookNum);
+			
+			$(this).children(".book").css("margin-left",  (r / i / 2) + "px");
+			$(this).children(".book").css("margin-right", (r / i / 2) + "px");
+			
+			//Width of one book element with padding
+			var bookOuterWidth = $(this).children(".book").outerWidth(true);
+			//Total width of all book elements with padding
+			var totalWidth = bookOuterWidth * bookNum;
+			
+			console.log(this.classList[1] + " container\nContainer width: " + containerWidth + "px\nBook width: " + bookInnerWidth + "px\nFits: " + i + "/" + bookNum + " books at once\nRemainder: " + r + "px\nMargins: " + r + "/" + i + "=" + (r / i / 2) + "px");
+		})
 	}
 	
 	window.onresize = setMargins;
