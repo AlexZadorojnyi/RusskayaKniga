0aa87cf545377c8f858d3b2f80f6c37ef8d2e6aa
Alexander Zadorojnyi <azadoroj.scs.ryerson.ca>
2018-07-12 15:27:15 -0400

    Added ability promos to be filtered by date and sorted by importance

--- js/myApp.js
+++ js/myApp.js
@@ -8,13 +8,30 @@ app.directive('cycleSlides', function() {
 	};
 });
 
+//Filters promos based on start and end dates
+app.filter('promoFilter', function(){
+	return function(input){
+		var output = [];
+		var currentDate = new Date();
+		
+		for(var i = 0; i < input.length; i++){
+			//Begins displaying on start date, removed on end date
+			if(input[i].endDate.getTime() > currentDate.getTime() && currentDate.getTime() > input[i].startDate.getTime()){
+				output.push(input[i]);
+			}
+		}
+		return output;
+	}
+});
+
 app.controller('myCtrl', ['$scope', function($scope) {
 	angular.element(document).ready(function () {
-        //Width of one book element without padding
-		var bookInnerWidth = $(".book").outerWidth();
-		
+        //Scroll variables
 		var blockScroll = false;
 		var scrollSpeed = 400;
+		
+		//Width of one book element without padding
+		var bookInnerWidth = $(".book").outerWidth();
 		
 		//Sets margin width based on container parameters
 		setMargins();
@@ -25,19 +42,20 @@ app.controller('myCtrl', ['$scope', function($scope) {
 			var thisClass = this.classList[1];
 			if(!reachedStart(thisClass)){
 				if(!blockScroll){
-					console.log("Scroll Left");
 					blockScroll = true;
+					//Animate for first book in the slider, buttons toggle when animation completes
 					$("." + thisClass + ".book:first").animate({right: "-=" + $("." + thisClass + ".book").outerWidth(true) + "px"}, scrollSpeed, function(){
 						blockScroll = false;
-						if(reachedStart(thisClass)) toggleButton(thisClass, "left", "off");
+						if(reachedStart(thisClass)){
+							toggleButton(thisClass, "left", "off");
+							console.log(thisClass + " reached start of list");
+						}
 						if(!reachedEnd(thisClass)) toggleButton(thisClass, "right", "on");
 						//console.log(Math.round($("." + thisClass + ".book:first").position().left - 1));
 					});
+					//Animate for the rest of the books in the slider
 					$("." + thisClass + ".book:not(:first)").animate({right: "-=" + $("." + thisClass + ".book").outerWidth(true) + "px"}, scrollSpeed);
 				}
-			} else {
-				toggleButton(thisClass, "left", "off");
-				console.log(thisClass + " reached start of list");
 			}
 		});
 		
@@ -46,19 +64,20 @@ app.controller('myCtrl', ['$scope', function($scope) {
 			var thisClass = this.classList[1];
 			if(!reachedEnd(thisClass)){
 				if(!blockScroll){
-					console.log("Scroll Right");
 					blockScroll = true;
+					//Animate for first book in the slider, buttons toggle when animation completes
 					$("." + thisClass + ".book:first").animate({right: "+=" + $("." + thisClass + ".book").outerWidth(true) + "px"}, scrollSpeed, function(){
 						blockScroll = false;
-						if(reachedEnd(thisClass)) toggleButton(thisClass, "right", "off");
+						if(reachedEnd(thisClass)){
+							toggleButton(thisClass, "right", "off");
+							console.log(thisClass + " reached end of list");
+						}
 						if(!reachedStart(thisClass)) toggleButton(thisClass, "left", "on");
 						//console.log(Math.round($("." + thisClass + ".book:first").position().left - 1));
 					});
+					//Animate for the rest of the books in the slider
 					$("." + thisClass + ".book:not(:first)").animate({right: "+=" + $("." + thisClass + ".book").outerWidth(true) + "px"}, scrollSpeed);
 				}
-			} else {
-				toggleButton(thisClass, "right", "off");
-				console.log(thisClass + " reached end of list");
 			}
 		});
 		
@@ -106,13 +125,11 @@ app.controller('myCtrl', ['$scope', function($scope) {
 			var bookOuterWidth = $("." + thisClass + ".book").outerWidth(true);
 			var bookNum = $("." + thisClass + ".book").length;
 			var containerWidth = $("." + thisClass + ".bookContainer").innerWidth();
-			var bookPos = $("." + thisClass + ".book:first").position().left - 1;
+			var bookPos = $("." + thisClass + ".book:first").position().left;
 			
 			if(Math.round(bookOuterWidth * bookNum - containerWidth + bookPos - bookOuterWidth) >= 0){
-				//$(".scrollRight."  + thisClass).animate({opacity: "1"});
 				return false;
 			} else {
-				//$(".scrollRight."  + thisClass).animate({opacity: "0"});
 				return true;
 			}
 		}
@@ -125,10 +142,8 @@ app.controller('myCtrl', ['$scope', function($scope) {
 			var bookPos = $("." + thisClass + ".book:first").position().left - 1;
 			
 			if(Math.round(bookPos + bookOuterWidth) <= 0){
-				//toggleButton(thisClass, "left", "on");
 				return false;
 			} else {
-				//toggleButton(thisClass, "left", "off");
 				return true;
 			}
 		}
@@ -149,87 +164,101 @@ app.controller('myCtrl', ['$scope', function($scope) {
 		function toggleButton(thisClass, button, toggle){
 			if(button == "right"){
 				if(toggle == "on"){
-					$(".scrollRight." + thisClass).animate({opacity: "1"});
+					$(".scrollRight." + thisClass).animate({opacity: "1"}, "fast");
 					$(".scrollRight." + thisClass).css("cursor", "pointer");
 					//console.log(thisClass + " " + button + " " + toggle);
 				} else if(toggle == "off"){
-					$(".scrollRight." + thisClass).animate({opacity: "0"});
+					$(".scrollRight." + thisClass).animate({opacity: "0"}, "fast");
 					$(".scrollRight." + thisClass).css("cursor", "default");
 					//console.log(thisClass + " " + button + " " + toggle);
 				}
 			} else if (button == "left") {
 				if(toggle == "on"){
-					$(".scrollLeft." + thisClass).animate({opacity: "1"});
+					$(".scrollLeft." + thisClass).animate({opacity: "1"}, "fast");
 					$(".scrollLeft." + thisClass).css("cursor", "pointer");
 					//console.log(thisClass + " " + button + " " + toggle);
 				} else if(toggle == "off"){
-					$(".scrollLeft." + thisClass).animate({opacity: "0"});
+					$(".scrollLeft." + thisClass).animate({opacity: "0"}, "fast");
 					$(".scrollLeft." + thisClass).css("cursor", "default");
 					//console.log(thisClass + " " + button + " " + toggle);
 				}
 			}
 		}
 	});
+	$scope.random = function(){
+		return 0.5 - Math.random();
+	};
 	$scope.promos = [
   	{ 
 		address: "../../images/promo/eng/promoEng5.jpg", 
-		date: new Date('2018', '06', '19'),
-		lang: "en"
+		startDate: new Date('2018-07-11'),
+		endDate: new Date('2018-09-12'),
+		lang: "en",
+		importance: 3
   	},
 	{ 
 		address: "../../images/promo/eng/promoEng6.jpg", 
-		date: new Date('2018', '06', '20'),
-		lang: "en"
+		startDate: new Date('2018-06-20'),
+		endDate: new Date('2018-08-01'),
+		lang: "en",
+		importance: 2
   	},
 	{ 
 		address: "../../images/promo/eng/promoEng7.jpg", 
-		date: new Date('2018', '06', '21'),
-		lang: "en"
+		startDate: new Date('2018-06-21'),
+		endDate: new Date('2018-08-01'),
+		lang: "en",
+		importance: 1
   	},
 	{ 
 		address: "../../images/promo/rus/promoRus5.jpg", 
-		date: new Date('2018', '06', '19'),
-		lang: "ru"
+		startDate: new Date('2018-06-19'),
+		endDate: new Date('2018-08-01'),
+		lang: "ru",
+		importance: 3
   	},
 	{ 
 		address: "../../images/promo/rus/promoRus6.jpg", 
-		date: new Date('2018', '06', '20'),
-		lang: "ru"
+		startDate: new Date('2018-06-20'),
+		endDate: new Date('2018-08-01'),
+		lang: "ru",
+		importance: 2
   	},
 	{ 
 		address: "../../images/promo/rus/promoRus7.jpg", 
-		date: new Date('2018', '06', '21'),
-		lang: "ru"
-  	}
-  ];
+		startDate: new Date('2018-06-21'),
+		endDate: new Date('2018-08-01'),
+		lang: "ru",
+		importance: 1
+  	}];
 	$scope.books = [
 	{
 		address: "../../images/books/01.jpg",
 		title: "История Российского государства. Царь Петр Алексеевич. Азиатская европеизация",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/02.jpg",
 		title: "Ореховый Будда",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/03.jpg",
 		title: "Призрак Канта",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/04.jpg",
 		title: "Не прощаюсь",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 		
 	},
 	{
@@ -237,182 +266,181 @@ app.controller('myCtrl', ['$scope', function($scope) {
 		title: "Огонь и агония",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/06.jpg",
 		title: "Дети мои",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/07.jpg",
 		title: "Тобол. Много званых",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/08.jpg",
 		title: "Вафли по-шпионски",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/09.jpg",
 		title: "iPhuck 10",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals", "Top Sellers"]
 	},
 	{
 		address: "../../images/books/10.jpg",
 		title: "Дальше жить",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/11.jpg",
 		title: "Самые родные, самые близкие",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/12.jpg",
 		title: "Про девушку, которая была бабушкой",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/13.jpg",
 		title: "Тобол. Мало избранных",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/14.jpg",
 		title: "Бабий ветер",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals", "Top Sellers"]
 	},
 	{
 		address: "../../images/books/15.jpg",
 		title: "Русская фантастика-2017. Том первый",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/16.jpg",
 		title: "Погребенный великан",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/17.jpg",
 		title: "Шпионы тоже лохи",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/18.jpg",
 		title: "Джек Ричер, или Без второго имени",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/19.png",
 		title: "Мой дикий сад",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/20.jpg",
 		title: "Желтые розы для актрисы",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/21.jpg",
 		title: "Крайон. Обретение счастья. Победа над страхами, тревогами, сомнениями",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/22.jpg",
 		title: "Всё та же я. Цикл До встречи с тобой. Кн.3",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/23.jpg",
 		title: "Очаг",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/24.jpg",
 		title: "След лисицы на камнях",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/25.jpg",
 		title: "Я признаюсь",
 		price: 0 ,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals"]
 	},
 	{
 		address: "../../images/books/26.jpg",
 		title: "Девушка не нашего круга",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals", "Top Sellers"]
 	},
 	{
 		address: "../../images/books/27.jpg",
 		title: "Союз радости и печали",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals", "Top Sellers"]
 	},
 	{
 		address: "../../images/books/28.jpg",
 		title: "Запасной выход из комы",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals", "Top Sellers"]
 	},
 	{
 		address: "../../images/books/29.jpg",
 		title: "Венец демона",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
+		category: ["New Arrivals", "Top Sellers"]
 	},
 	{
 		address: "../../images/books/30.jpg",
 		title: "Север и Юг",
 		price: 0,
 		author: "",
-		category: "New Arrivals"
-	}
-  ];
+		category: ["New Arrivals", "Top Sellers"]
+	}];
 }]);
